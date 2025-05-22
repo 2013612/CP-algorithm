34a38d4dbc638fb51736ad984bbfe6d4eaa94eb9
2013612 <samuelwong2013612@gmail.com>
2025-05-23 02:11:14 +0800

    codeforces 2102E

--- codeforces/2102/2102E.cpp
+++ codeforces/2102/2102E.cpp
@@ -3,98 +3,63 @@ using namespace std;
 
 int n, a[300006];
 long long ans;
-
-long long f(vector<int> v1, vector<int> v2) {
-	long long res = 0;
-	
-	vector<int> v3 = v1, v4 = v2;
-	sort(v1.begin(), v1.end());
-	sort(v2.begin(), v2.end());
-	
-	int cur = 1e9;
-	int l = v1.size() - 1;
-	int r = v2.size() - 1;
-	set<int> s1, s2;
-	
-	while (l >= 0 && r >= 0) {
-		int k = min(v1[l], v2[r]);
-		if (k == cur) {
-			k--;
-		}
-		
-		cur = k;
-		
-		if (cur == 0) {
-			break;
-		}
-		
-		s1.insert(cur);
-		l--;
-		r--;
-	}
-	
-	{
-		s2 = s1;
-		
-		for (int i = 0; i < v1.size() && s1.size(); i++) {
-			auto x = s1.upper_bound(v3[i]);
-			
-			if (x == s1.begin()) {
-				continue;
-			}
-			x--;
-			
-//			printf("!! %d %d %d\n", i, *x, v3[i]);
-			s1.erase(x);
-
-			res -= i;
-		}
-		
-//		printf("res: %lld\n", res);
-		
-		for (int i = v2.size() - 1; i >= 0 && s2.size(); i--) {
-			auto x = s2.upper_bound(v4[i]);
-			
-			if (x == s2.begin()) {
-				continue;
-			}
-			x--;
-			
-			s2.erase(x);
-			res += v1.size() + i;
-		}
-	}
-	
-	return res;
-}
+set<int> s[2];
 
 void solve() {
 	scanf("%d", &n);
+	s[0].clear();
+	s[1].clear();
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &a[i]);
 	}
 	
-	vector<int> v1, v2;
-	for (int i = 0; i < n / 2; i++) {
-		v1.push_back(a[i]);
-	}
-	for (int i = n / 2; i < n; i++) {
-		v2.push_back(a[i]);
+	for (int i = 1; i <= n / 2 + 1; i++) {
+		s[0].insert(i);
+		s[1].insert(i);
 	}
 	
-	ans = f(v1, v2);
+	ans = 0;
+	long long t = 0;
 	
-	if (n & 1) {
-		v1.clear();
-		v2.clear();
-		for (int i = 0; i < n / 2 + 1; i++) {
-			v1.push_back(a[i]);
-		}
-		for (int i = n / 2 + 1; i < n; i++) {
-			v2.push_back(a[i]);
+	int cur = 1;
+	int l = 0, r = n - 1;
+	
+	while (l <= r) {
+		while (a[l] < *s[0].begin()) {
+			l++;
 		}
 		
-		ans = max(ans, f(v1, v2));
+		if (l > r) {
+			break;
+		}
+		
+		{
+			auto x = s[0].upper_bound(a[l]);
+			x--;
+			s[0].erase(x);
+			
+			t -= l;
+			l++;
+		}
+		
+		while (a[r] < *s[1].begin()) {
+			r--;
+		}
+		
+		if (l > r) {
+			break;
+		}
+		
+		{
+			auto x = s[1].upper_bound(a[r]);
+			x--;
+			s[1].erase(x);
+			
+			t += r;
+			r--;
+		}
+		
+		ans = max(ans, t);
 	}
 	
 	printf("%lld\n", ans);
